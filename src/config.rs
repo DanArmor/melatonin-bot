5d27d87630c0b0f0fc1d39669f251f6ac4e255a7
DanArmor <39347109+DanArmor@users.noreply.github.com>
2023-08-01 23:10:17 +0300

    feat: botState and connection pool

---
+++ src/config.rs
@@ -0,0 +1,61 @@
+use log::{debug, error, info};
+use mobot::BotState;
+use serde::Deserialize;
+use sqlx::migrate::MigrateDatabase;
+use sqlx::sqlite::SqlitePoolOptions;
+use sqlx::{Pool, Sqlite};
+use tokio::sync::OnceCell;
+
+#[derive(Deserialize, Clone, Debug, Default)]
+pub struct Config {
+    pub holodex_api_key: String,
+    pub telegram_bot_token: String,
+    pub sql_connection_string: String,
+    pub max_connections: u32,
+}
+
+#[derive(Debug, Clone, Default, BotState)]
+pub struct MelatoninBotState {
+    config: Config,
+    sql_pool: MyPool,
+}
+
+impl MelatoninBotState {
+    pub fn new(config: Config) -> Self {
+        MelatoninBotState {
+            config: config,
+            sql_pool: MyPool::default(),
+        }
+    }
+}
+
+static POOL: OnceCell<Pool<Sqlite>> = OnceCell::const_new();
+
+pub async fn init_db(db_path: String, max_conn: u32) -> Result<(), anyhow::Error> {
+    if !Sqlite::database_exists(&db_path).await.unwrap_or(false) {
+        match Sqlite::create_database(&db_path).await {
+            Ok(_) => info!("{} was created", db_path),
+            Err(e) => error!("{}", e),
+        }
+    } else {
+        info!("{} already exists", db_path)
+    }
+    let pool = SqlitePoolOptions::new()
+        .max_connections(max_conn)
+        .connect(&db_path)
+        .await?;
+    POOL.set(pool)?;
+    Ok(())
+}
+
+#[derive(Debug, Clone)]
+struct MyPool(Pool<Sqlite>);
+
+unsafe impl Send for MyPool {}
+unsafe impl Sync for MyPool {}
+
+impl Default for MyPool {
+    fn default() -> Self {
+        MyPool(POOL.get().unwrap().clone())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use log::{debug, error, info};
+use sqlx::sqlite::SqlitePoolOptions;
+use sqlx::Sqlite;
+use sqlx::{any, migrate::MigrateDatabase};
+use std::fs;
+
+mod config;
+use config::*;
+
+pub fn init_logger() {
+    let _ = env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info"))
+        .try_init();
+    debug!("Logger initialized.");
+}
+
+pub fn init_bot_state(config: Config) -> MelatoninBotState {
+    let state = MelatoninBotState::new(config);
+    state
+}
+
+fn init_config() -> Config {
+    let data = fs::read_to_string("dev.json").unwrap();
+    serde_json::from_str(&data).unwrap()
+}
+
+pub async fn init_app() -> anyhow::Result<MelatoninBotState> {
+    init_logger();
+    let config = init_config();
+
+    init_db(config.sql_connection_string.clone(), config.max_connections).await?;
+    let state = init_bot_state(config);
+    Ok(state)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    init_app().await?;
+
+    Ok(())
 }
