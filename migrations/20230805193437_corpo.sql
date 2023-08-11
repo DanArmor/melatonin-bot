103d65d9c1955f537efb02cffb65a1acc7402a52
DanArmor <39347109+DanArmor@users.noreply.github.com>
2023-08-11 16:12:25 +0300

    feat: scheme changed

--- migrations/20230805193437_corpo.sql
+++
@@ -1,5 +0,0 @@
--- Add migration script here
-CREATE TABLE IF NOT EXISTS corpo (
-    id INTEGER PRIMARY KEY NOT NULL,
-    name VARCHAR(256) NOT NULL
-);
--- migrations/20230805193442_vtuber.sql
+++ migrations/20230805193442_vtuber.sql
@@ -3,8 +3,7 @@ CREATE TABLE IF NOT EXISTS vtuber (
     id INTEGER PRIMARY KEY NOT NULL,
     first_name VARCHAR(256) NOT NULL,
     last_name VARCHAR(256) NOT NULL,
-    username VARCHAR(256) NOT NULL,
-    youtube_id VARCHAR(256) NOT NULL,
-    corpo_id INTEGER NOT NULL,
-    language_code VARCHAR(256) NOT NULL
+    emoji VARCHAR(256) NOT NULL,
+    youtube_handle VARCHAR(256) NOT NULL,
+    youtube_channel_id VARCHAR(256) NOT NULL
 );
