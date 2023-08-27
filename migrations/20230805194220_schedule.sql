708fc014d4d96af45775ff78061934d7b11d9aa9
DanArmor <39347109+DanArmor@users.noreply.github.com>
2023-08-27 16:13:48 +0300

    feat: migration changes

--- migrations/20230805194220_schedule.sql
+++ migrations/20230805194220_schedule.sql
@@ -1,8 +1,8 @@
 -- Add migration script here
-CREATE TABLE IF NOT EXISTS user_vtuber (
+CREATE TABLE IF NOT EXISTS reported_stream (
     id INTEGER PRIMARY KEY NOT NULL,
+    video_id VARCHAR(64) NOT NULL,
     vtuber_id INTEGER NOT NULL,
     scheduled_start DATETIME NOT NULL,
-    video_link VARCHAR(256) NOT NULL,
     FOREIGN KEY(vtuber_id) REFERENCES vtuber(id)
 );
